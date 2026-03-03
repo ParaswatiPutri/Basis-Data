2796ad9cfbb9261ff44782ef7481b9e0120fa87a
ParaswatiPutri <paraswati90965@gmail.com>
2026-03-03 23:32:35 +0700

    Add files via upload

--- TugasModul3_DBMS_2505060059.sql
+++ TugasModul3_DBMS_2505060059.sql
@@ -1,6 +1,6 @@
 -- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
 --
--- Host: localhost    Database: biodata_mahasiswa
+-- Host: localhost    Database: data_mahasiswa
 -- ------------------------------------------------------
 -- Server version	10.4.32-MariaDB
 
@@ -14,3 +14,42 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `biodata_mahasiswa`
+--
+
+DROP TABLE IF EXISTS `biodata_mahasiswa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `biodata_mahasiswa` (
+  `NPM` char(5) NOT NULL,
+  `Nama` varchar(25) NOT NULL,
+  `Tempat_Lahir` varchar(30) NOT NULL,
+  `Tanggal_Lahir` date NOT NULL,
+  `Jenis_Kelamin` enum('L','P') NOT NULL,
+  `NO_Hp` varchar(25) NOT NULL,
+  PRIMARY KEY (`NPM`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `biodata_mahasiswa`
+--
+
+LOCK TABLES `biodata_mahasiswa` WRITE;
+/*!40000 ALTER TABLE `biodata_mahasiswa` DISABLE KEYS */;
+INSERT INTO `biodata_mahasiswa` VALUES ('12345','JENNIE','BANDUNG','2007-02-16','P','0854675467'),('12346','JAEMIN','BANDUNG','2002-12-22','L','0865476865'),('12347','LALISA MANOBAN','BANDUNG','2007-03-27','P','085424864215'),('12349','ROSE','MAKASAR','2006-04-17','P','085790864352');
+/*!40000 ALTER TABLE `biodata_mahasiswa` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-03 23:32:00
